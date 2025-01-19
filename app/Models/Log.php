a7eee24f9c1b659b4f62ada41155a709ff541c1e
Wahyu Tri Budi Pangestu <wahyutbp28@students.unnes.ac.id>
2025-01-19 13:06:00 +0700

    Perbaikan Log Model

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -10,5 +10,10 @@ class Log extends Model
     use HasFactory;
 
     protected $fillable = ['task_id', 'action'];
-}
 
+    // Tambahkan relasi ke model Task
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+}
